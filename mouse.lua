3ccbad149ddcbfd641e14cf04ca5023b21da41ed
heymans <heymanswang@outlook.com>
2025-04-02 14:12:09 +0800

    更新鼠标配置，右键拖拽选择文本，修正鼠标模式配置项为 enable_kitty_keyboard

--- mouse.lua
+++ mouse.lua
@@ -6,19 +6,10 @@ local M = {}
 function M.apply_to_config(config)
   -- 鼠标映射配置
   config.mouse_bindings = {
-    -- 右键行为：如果有选择文本则复制，否则粘贴
+    -- 右键拖拽选择
     {
-      event = { Down = { streak = 1, button = 'Right' } },
-      mods = 'NONE',
-      action = wezterm.action_callback(function(window, pane)
-        local has_selection = window:get_selection_text_for_pane(pane) ~= ''
-        if has_selection then
-          window:perform_action(act.CopyTo('ClipboardAndPrimarySelection'), pane)
-          window:perform_action(act.ClearSelection, pane)
-        else
-          window:perform_action(act.PasteFrom('Clipboard'), pane)
-        end
-      end),
+      event = { Drag = { streak = 1, button = "Right" } },
+      action = wezterm.action { ExtendSelectionToMouseCursor = "Cell" }
     },
     
     -- 保留默认的左键选择行为
@@ -47,8 +38,9 @@ function M.apply_to_config(config)
     },
   }
   
-  -- 启用鼠标模式，使 vim 可以接收鼠标事件
-  config.enable_kitty_mouse = true  -- 使用正确的配置项名称
+  -- 启用正确的鼠标模式配置
+  -- 使用 enable_kitty_keyboard 代替不存在的 enable_kitty_mouse
+  config.enable_kitty_keyboard = true
   
   -- 允许应用程序覆盖光标样式
   config.allow_win32_input_mode = true
